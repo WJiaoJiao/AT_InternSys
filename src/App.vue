ce593635bd5d1dbe88f7ff708a4e20be451584bd
wjiaojiao <951773099@qq.com>
2018-01-17 10:31:16 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <header>
       <app-nav></app-nav>
     </header>
+    <div>test</div>
     <main class="container">
       <div style="margin-top:20px"><app-breadcrumbs/></div>
       <transition name="el-fade-in-linear">
